f7c3a922c97fdcf261824289959b1c1c9b3a2bdc
eucar2026 <eucar2026@francisparker.org>
2024-12-31 16:28:23 -0800

    standard LlamaIndex RAG proof of concept

--- query.py
+++ query.py
@@ -1,43 +1,12 @@
 import os
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext
-
-from llama_index.core.storage.docstore import SimpleDocumentStore
-from llama_index.graph_stores.neo4j import Neo4jPGStore
-from llama_index.core.storage.index_store import SimpleIndexStore
-
-from llama_index.core import (
-    load_index_from_storage,
-    load_indices_from_storage,
-    load_graph_from_storage,
-)
+from llama_index.core import StorageContext, load_index_from_storage
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#documents = SimpleDirectoryReader("data").load_data()
-#index = VectorStoreIndex.from_documents(documents)
-
-#from llama_index.indices.propterty_graph import SchemaLLMPathExtractor
-
-graph_store = Neo4jPGStore(
-    username="prog2",
-    password=os.environ["NEO4J_PASSWORD"],
-    url="bolt://localhost:7687",
-    database="neo4j",
-)
-
-storage_context = StorageContext.from_defaults(
-    docstore=SimpleDocumentStore.from_persist_dir(),
-    graph_store=graph_store,
-    #index_store=SimpleIndexStore.from_persist_dir(),
-)
-
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
 index = load_index_from_storage(storage_context)
-
-query_engine = index.as_query_engine(
-    include_text=False, response_mode="tree_summarize",
-    verbose=True
-)
-response = query_engine.query("When was react rewritten? ")
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do after he came to the U.S.? ")
 print(response)
