c980b5cec17ea9c5200d168226619ad9d09a5d08
eucar2026 <eucar2026@francisparker.org>
2025-02-16 17:54:07 -0800

    fixed null character issue when indexing through pypdf2

--- server/test/add_files.py
+++ server/test/add_files.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
-from llama_index.core import SimpleDirectoryReader
+from llama_index.core import Document
+import PyPDF2
+from pathlib import Path
 import os.path
 import os
 # setting path to import postgres library
@@ -13,17 +15,24 @@ def add_files(dbName):
     """adds file to selected database and indexes it
     upload file api call is supposed to save the pdf inside the database's temp folder
     """
-    # loads documents that were put in temp folder by file upload api
-    documents = SimpleDirectoryReader(f"/databases/{dbName}/temp", filename_as_id=True).load_data()
-    print(documents)
-
-    # insert documents to database
-    for doc in documents:
-        index_document(doc)
-    
-    # moves indexed files to files folder
+    # process the files in temp folder one by one
     fileList = os.listdir(f"/databases/{dbName}/temp")
     for file in fileList:
-        os.rename(f"/databases/{dbName}/temp/{file}", f"/databases/{dbName}/files/{file}")
+        # loads documents that were put in temp folder by file upload api
+        file_path = f"/databases/{dbName}/temp/{file}"
+        pdf_reader = PyPDF2.PdfReader(file_path)
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text = page.extract_text()
+            document = Document(
+                text=text.replace('\x00', ''),
+                id_=f"{file}_part_{page_num}"
+            )
+
+            # insert documents to database
+            index_document(document)
+        
+        # moves indexed files to files folder
+        os.rename(file_path, f"/databases/{dbName}/files/{file}")
 
 add_files("test")
